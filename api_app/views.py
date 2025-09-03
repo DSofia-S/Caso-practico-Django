167427fb81354977982bb83e1a700903a46e6308
DSofia-S <zerausannad@gmail.com>
2025-09-02 21:46:24 -0500

    Se agregan nuevos endpoints a las views.py

--- api_app/views.py
+++ api_app/views.py
@@ -97,3 +97,53 @@ class CrearTarea(generics.CreateAPIView):
             raise NotFound("No se encontraron tareas para ese rango de fechas.")
         serializer = TareaSerializer(tareas, many=True)
         return Response({'success': True, 'detail': 'Tareas encontradas', 'data': serializer.data}, status=status.HTTP_200_OK)
+    
+class ActualizarTarea(generics.UpdateAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+
+    def put(self, request, pk):
+        tarea = get_object_or_404(Tarea, pk=pk)
+        serializer = TareaSerializer(tarea, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'success': True, 'detail': 'Tarea actualizada con éxito', 'data': serializer.data}, status=status.HTTP_200_OK)
+
+class EliminarTarea(generics.DestroyAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+
+    def delete(self, request, pk):
+        tarea = get_object_or_404(Tarea, pk=pk)
+        tarea.delete()
+        return Response({'success': True, 'detail': 'Tarea eliminada con éxito'}, status=status.HTTP_204_NO_CONTENT)
+
+class TareasPorFecha(generics.ListAPIView):
+    serializer_class = TareaSerializer
+
+    def get(self, request, fecha_limite):
+        tareas = Tarea.objects.filter(fecha_limite=fecha_limite).first()
+        if not tareas:
+            raise NotFound("No se encontraron tareas para esa fecha.")
+        serializer = TareaSerializer(tareas)
+        return Response({'success': True, 'detail': 'Tareas encontradas', 'data': serializer.data}, status=status.HTTP_200_OK)
+    
+class TareasPorPersona(generics.ListAPIView):
+    serializer_class = TareaSerializer
+
+    def get(self, request, documento):
+        tareas = Tarea.objects.filter(persona__documento=documento).first()
+        if not tareas:
+            raise NotFound("No se encontraron tareas para ese documento.")
+        serializer = TareaSerializer(tareas)
+        return Response({'success': True, 'detail': 'Tareas encontradas', 'data': serializer.data}, status=status.HTTP_200_OK)
+
+class TareasPorRango(generics.ListAPIView):
+    serializer_class = TareaSerializer
+
+    def get(self, request, fecha_inicio, fecha_fin):
+        tareas = Tarea.objects.filter(fecha_limite__range=[fecha_inicio, fecha_fin])
+        if not tareas:
+            raise NotFound("No se encontraron tareas para ese rango de fechas.")
+        serializer = TareaSerializer(tareas, many=True)
+        return Response({'success': True, 'detail': 'Tareas encontradas', 'data': serializer.data}, status=status.HTTP_200_OK)
